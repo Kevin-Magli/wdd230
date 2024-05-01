c82c966dd8d67c654f7e6d4a298330268032600a
kevin-magli <kmagliocchi@gmail.com>
2024-04-30 22:00:29 -0300

    finished home page

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+let year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+
+let updated = document.lastModified;
+document.getElementById("lastMod").innerHTML = updated;
