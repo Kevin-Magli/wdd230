6a161a33d83bf5994618cd19510bda6ee24733f7
kevin-magli <kmagliocchi@gmail.com>
2024-05-09 11:40:20 -0300

    Theme auto-set

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Hamburger menu
+// #Hamburger menu
 const menuButton = document.getElementById('menu');
 const navigation = document.getElementById('navigation');
 
@@ -7,9 +7,21 @@ menuButton.addEventListener('click', () => {
   menuButton.classList.toggle('active');
 });
 
-// Dark Mode button
+// #Dark Mode button
 const darkButton = document.getElementById('dark');
 
+// auto set dark mode based on browser preference
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+  darkButton.classList.add('active');
+
+  document.body.style.backgroundColor = '#1E1E1E';
+  document.body.style.color = '#D4D4D4';
+  document.querySelectorAll('a').forEach(link => {
+    link.style.color = '#3498db';
+  });
+}
+
+// button config
 darkButton.addEventListener('click', () => {
   darkButton.classList.toggle('active');
 
