1612a6c2754eee1e3df84133aa2fd98ea8bc8b99
Kevin-Magli <kmagliocchi@gmail.com>
2024-05-27 16:42:47 -0300

    Changed the discover page to the intended layout

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -6,3 +6,59 @@ hamMenu.addEventListener('click', () => {
   hamMenu.classList.toggle('active');
   offScreenMenu.classList.toggle('active');
 })
+
+
+
+function updateWelcome() {
+  let visitCountKey = 'visits_' + location.hostname + '_welcome'; 
+  let lastVisitDateKey = 'lastVisit_' + location.hostname + '_welcome';
+  let visitCount = localStorage.getItem(visitCountKey);
+  let fullDay = new Date();
+
+  if (visitCount === null) {
+    visitCount = 0;
+  } else {
+    visitCount = parseInt(visitCount);
+  }
+  visitCount++;  
+  localStorage.setItem(visitCountKey, visitCount);
+
+  let lastVisitDate = localStorage.getItem(lastVisitDateKey);
+  if (lastVisitDate !== null) {
+    let timeBetweenVisits = fullDay.getTime() - new Date(lastVisitDate).getTime();
+    let daysBetweenVisits = Math.ceil(timeBetweenVisits / (1000 * 60 * 60 * 24));
+
+    if (visitCount > 1) {
+      if (lastVisitDate !== null) {
+        let timeBetweenVisits = fullDay.getTime() - new Date(lastVisitDate).getTime();
+        let daysBetweenVisits = Math.ceil(timeBetweenVisits / (1000 * 60 * 60 * 24));
+    
+        if (daysBetweenVisits < 1) {
+          console.log("Back so soon! Awesome!");
+        } else {
+          document.getElementById('welcome-visitor').textContent = ("You last visited " + daysBetweenVisits + " day" + (daysBetweenVisits > 1 ? "s" : "") + " ago.");
+        }
+      }
+    }
+  }
+  // Update the last visit date
+  localStorage.setItem(lastVisitDateKey, fullDay.toISOString());
+
+  console.log(visitCount);
+  console.log(fullDay);
+}
+
+
+
+
+
+
+
+updateWelcome();
+
+
+
+
+
+
+updateVisitCount();
