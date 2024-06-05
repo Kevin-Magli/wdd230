a306d4f9c6c18fcf822ab46d8edc5849c8748360
Kevin-Magli <kmagliocchi@gmail.com>
2024-06-05 18:04:09 -0300

    added weather information

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,8 +1,8 @@
-const temp = document.querySelector('#temp');
-const icon = document.querySelector('#weather-icon');
-const condition = document.querySelector('#condition');
+const icon = document.getElementById('weather-icon');
+const temp = document.getElementById('weather-temp');
+const condition = document.getElementById('weather-condition');
 
--23.68275281401005, -46.572825967618954
+// -23.68275281401005, -46.572825967618954
 
 const url = `https://api.openweathermap.org/data/2.5/weather?lat=-23.68&lon=-46.572&units=metric&appid=7fcc85b72c8797be6d3dacecd03ef862`;
 
@@ -11,23 +11,24 @@ async function apiFetch() {
         const response = await fetch(url);
         if (response.ok) {
             const data = await response.json();
-            console.log(data); // this is for testing the call
+            console.log(data);
             displayResults(data);
-        } else {
+        }else{
             throw Error(await response.text());
         }
-    } catch (error) {
-        console.log(error);
+    }
+    catch (error) {
+        console.error(error);
     }
 }
 
-apiFetch();
 
 function displayResults(data) {
-    temp.innerHTML = `${data.main.temp}&deg;C`;
-    const iconsrc = `https://openweathermap.org/img/w/${icon}@2x.png`;
-    let desc = data.weather[0].description;
-    icon.setAttribute('src', iconsrc);
-    icon.setAttribute('alt', desc);
-    condition.innerHTML = `${desc}`;
+    dataIcon = data.weather[0].icon;
+    icon.src = `https://openweathermap.org/img/wn/${dataIcon}@2x.png`;
+    temp.innerHTML = `🌡${data.main.temp}&deg;C`;
+    condition.innerHTML = `${data.weather[0].description}`
 }
+
+
+apiFetch();
